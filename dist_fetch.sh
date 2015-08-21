8440adaf3faa4c5a31f2ea15acdcc6405398ef6f
Liam Conrad <lconrad@base2.restorehc.com>
2015-08-21 11:24:04 -0500

    Add dist_fetch script to pull in FreeBSD-10.2 into known location

---
+++ dist_fetch.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+server=ftp.freebsd.org
+
+dir=pub/FreeBSD/releases/amd64/amd64/10.2-RELEASE
+dest=./release/10.2-RELEASE
+
+base="MANIFEST \
+	base.txz \
+	doc.txz \
+	games.txz \
+	kernel.txz \
+	lib32.txz \
+	ports.txz \
+	src.txz "
+
+get() {
+	ftplist=""
+	for item in $@ ; do
+		full_name=ftp://$server/$dir/$item
+		ftplist="$ftplist $full_name"
+	done
+
+	echo "About to get the following:"
+	echo "$ftplist"
+
+	ftp -o $dest $ftplist
+}
+
+get $base
